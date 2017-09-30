f1625a91fc7fa6c719faa4bd0954d002e01f4863
Lukasz Pasek <lukaszpasek@Lukaszs-MacBook-Pro.local>
2017-09-29 23:05:49 -0700

    Transparent Dither
    Initial check-in for the project showing how dither can be used for transparency

---
+++ Assets/BayerMatrixGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BayerMatrixGenerator : MonoBehaviour {
+
+	// 2 x 2 Bayer Matrix, it is used as a base to calculate power of 2 bayer matrices
+	public float[] mat = {0,3,2,1};
+	public float[] newMat = { };
+
+	public int matrixSize = 4;
+
+	[ContextMenu("Calculate Matrix")]
+	public void Calculate()
+	{
+		int targetSize = matrixSize;
+		int currentSize = 2;
+
+		newMat = mat;
+
+		while (targetSize != currentSize) {
+			
+			newMat = GenerateMatrix (newMat, currentSize, currentSize * 2);
+			currentSize *= 2;
+		}
+
+		GetComponent<Renderer> ().sharedMaterial.SetFloatArray ("_Array", newMat);
+		GetComponent<Renderer> ().sharedMaterial.SetFloat ("_ArrayLength", targetSize*targetSize);
+		GetComponent<Renderer> ().sharedMaterial.SetFloat ("_ArraySide", targetSize);
+	}
+
+	float[] GenerateMatrix(float[] prevMatrix, int prevSize, int size)
+	{
+		List<float> newMatrix = new List<float> ();
+		for (int i = 0; i < size * size; i++) {
+			newMatrix.Add(0);
+		}
+
+		for (int j = 0; j < prevSize; j++) 
+		{
+			for (int i = 0; i < prevSize; i++) 
+			{
+				float prevValue = prevMatrix[(i%prevSize) + ((j%prevSize) * prevSize)];
+				if (i < prevSize && j < prevSize)
+				{
+					newMatrix [(i + j * size)] = prevValue * 4;
+				}
+			}
+		}
+
+
+		for (int j = 0; j < size; j++) 
+		{
+			for (int i = 0; i < size; i++) 
+			{
+				float prevValue = newMatrix[(i%prevSize) + ((j%prevSize) * size)];
+				if (i < prevSize && j < prevSize)
+				{
+					// ignore this case, for simplicity pf the alghoritm it was pre-calculated during previous iteration
+				} 
+				else if (i >= prevSize && j < prevSize)
+				{
+					newMatrix [(i + j * size)] = prevValue + 3;
+				}
+				else if (j >= prevSize && i < prevSize)
+				{
+					newMatrix [(i + j * size)] = prevValue + 2;
+				}
+				else
+				{
+					newMatrix [(i + j * size)] = prevValue + 1;
+				}
+			}
+		}
+
+		return newMatrix.ToArray();
+	}
+}
---
+++ Assets/DitheredShader.shader
@@ -0,0 +1,79 @@
+﻿Shader "Unlit/DitheredShader"
+{
+	Properties
+	{
+		_Transparency ("Transparency", Float) = 0.5 
+		_Resolution ("Resolution", Int) = 512
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			// Transparency 0 to 1, 0 being transparent, 1 being opaque
+			float _Transparency;
+
+			// Current resolution of the material
+			int _Resolution;
+
+			// Side of the bayer matrix
+			int _ArraySide = 0;
+
+			// Total number of bayer matrix values
+			int _ArrayLength = 0;
+
+			// Matrix to store bayer matrix values
+			float _Array[256];
+
+			bool CheckIfDiscard(float threshold, int cordX, int cordY) {
+			    int x = cordX % _ArraySide;
+			    int y = cordY % _ArraySide;
+			    float result = _Array[(x + y * _ArraySide)] / _ArrayLength;
+			    return (result < threshold) ? false : true;
+			}
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = fixed4(0,0,1,1);
+
+				if(CheckIfDiscard(_Transparency, i.uv.y * _Resolution,i.uv.x * _Resolution))
+				{
+			   	 	discard;
+				}
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
